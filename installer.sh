e084efb4378405a255a960ce22a839c140a88ccf
Star_destroyer11 <stardestroyersd89@gmail.com>
2026-01-10 22:26:24 -0700

    Fix log messages by adding periods

--- installer.sh
+++ installer.sh
@@ -15,23 +15,19 @@ fi
 
 log "Starting MushM Installer"
 
-NONA_MOD_CONF="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/nonamod.conf"
-CONF_DIR="/etc/init/nonamod.conf"
 CROSH="/usr/bin/crosh"
 MURK_DIR="/mnt/stateful_partition/murkmod"
 MUSHM_URL="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/mushm.sh"
 BOOT_SKRIPT="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/bootmsg.sh"
 BOOT_SK_DIR="/sbin/chromeos_startup"
 
-log "Installing Needed Things And Shit"
+log "Installing Needed Things And Shit."
 mkdir -p "$MURK_DIR/plugins" "$MURK_DIR/pollen" || error "Failed To Installing Needed Things And Shit"
-touch "$CONF_DIV"
-curl -fsSLo "$CONF_DIR" "$NONA_MOD_CONF" || error "Failed to download Config"
 
-log "Installing MushM"
+log "Installing MushM."
 curl -fsSLo "$CROSH" "$MUSHM_URL" || error "Failed to download MushM"
 
-log "Fixing Shity Boot Msg"
+log "Fixing Shity Boot Msg."
 touch "$BOOT_SK_DIR"
 curl -fsSLo "$BOOT_SK_DIR" "$BOOT_SKRIPT" || error "Failed to fix boot msg"
 
